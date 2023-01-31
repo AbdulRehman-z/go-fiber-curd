d0de22382df2954a84cc8559edec4c8b34455094
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-01-31 22:33:38 +0500

    added models and curd routes

--- main.go
+++ main.go
@@ -7,24 +7,45 @@ import (
 )
 
 
-// route handlers
-func initialController(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+// Book struct (Model)
+
+type Book struct {
+	ID     int `json:"id"`
+	Name   string `json:"name"`
+	Pages  string `json:"pages"`
+    Author *Author `json:"author"`
 }
+
+type Author struct {
+	ID   int `json:"id"`
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
+
+
+var books []Book
+
+// route handlers
+
 
 func main() {
 	// Create new Fiber instance
 	app := fiber.New()
 
-
+    books = append(books, Book{ID: 1, Name: "The Alchemist", Pages: "200", Author: 
+	&Author{ID: 1, Name: "Paulo Coelho", Age: "50"}})
 	// Routes
-	app.Get("/", initialController)
+	app.Get("/books",getBooksController)
+	app.Get("/books/:id",getBookController)
+	app.post("/books",addBookController)
+	app.put("/books/:id",updateBookController)
+	app.delete("/books/:id",deleteBookController)
 
 	// Start server
-	if err := app.Listen(":9000"); err != nil {
+	if err := app.Listen(":8000"); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func checkErr(err error) {
