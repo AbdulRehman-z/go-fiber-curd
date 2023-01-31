25caaf9d281f43ba96a85b06cd0bcf50edf177c2
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-01-31 16:50:24 +0500

    added live-server

--- main.go
+++ main.go
@@ -6,15 +6,26 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// routes handlers
+
+func initialController(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
+
 func main() {
 	app := fiber.New()
 
-	err := app.Listen(":3000")
-	checkErr(err)
+	app.Get("/", initialController)
+
+	// Start server
+	if err := app.Listen(":9000"); err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func checkErr(err error) {
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
