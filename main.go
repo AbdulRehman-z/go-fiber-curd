af88c59b652b314ee8a5c2859f1a9fabf0a8eec9
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-01 17:27:37 +0500

    Added controllers for GET, POST

--- main.go
+++ main.go
@@ -4,48 +4,89 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 
 // Book struct (Model)
 
 type Book struct {
-	ID     int `json:"id"`
+	ID     string `json:"id"`
 	Name   string `json:"name"`
-	Pages  string `json:"pages"`
+	Pages  uint `json:"pages"`
     Author *Author `json:"author"`
 }
 
 type Author struct {
-	ID   int `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
-	Age  string `json:"age"`
+	Age  uint `json:"age"`
 }
-
-
 
 var books []Book
 
-// route handlers
+/** 
+Routes Handlers
+**/
+
+// Get all books
+func getBooksController(c *fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+// Get single book
+func getBookController(c *fiber.Ctx) error {
+	id := c.Params("id")
+for _, book := range books {
+	if book.ID == id {
+return c.JSON(book)
+ }}
+
+return c.Status(404).JSON(fiber.Map{"message": "Book not found"})
+}
+
+// Add new book
+func addBookController(c *fiber.Ctx) error {
+	var book Book
+	if err := c.BodyParser(&book); err != nil {
+		return err
+	}
+
+	books = append(books, book)
+	return c.Status(201).JSON(book)
+}
 
 
 func main() {
 	// Create new Fiber instance
 	app := fiber.New()
 
-    books = append(books, Book{ID: 1, Name: "The Alchemist", Pages: "200", Author: 
-	&Author{ID: 1, Name: "Paulo Coelho", Age: "50"}})
+   app.Use(logger.New())
+
+    books = append(books, Book{ID: "1", Name: "The Alchemist", Pages: 200, Author: 
+	&Author{ID: "1", Name: "Paulo Coelho", Age: 50}})
+
+    books = append(books,Book{ID:"2",Name:"The Monk Who Sold His Ferrari",Pages:300,Author: &Author{ID:"2",Name:"Robin Sharma",Age:40}})
+
+
 	// Routes
-	app.Get("/books",getBooksController)
-	app.Get("/books/:id",getBookController)
-	app.post("/books",addBookController)
-	app.put("/books/:id",updateBookController)
-	app.delete("/books/:id",deleteBookController)
+   app.Route("/api/v1/books",func(router fiber.Router) {
+	router.Get("/",getBooksController)
+	router.Get("/:id<min(0)>",getBookController)
+	router.Post("/addBook",addBookController)
+	// router.put("/books/:id",updateBookController)
+	// router.delete("/books/:id",deleteBookController)
+
+   })
+
+	// app.Get("/books",getBooksController)
+	// app.Get("/books/:id<min(0)>",getBookController)
+	// app.Post("/books",addBookController)
+	// // app.put("/books/:id",updateBookController)
+	// // app.delete("/books/:id",deleteBookController)
 
 	// Start server
-	if err := app.Listen(":8000"); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(app.Listen(":8000"))
 }
 
 func checkErr(err error) {
