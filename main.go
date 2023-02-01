efa929acd9981d5a3fe90d32e8a9fcf7ae67823d
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-01 17:58:27 +0500

    Added & tested Get, Del, Post successfully

--- main.go
+++ main.go
@@ -7,28 +7,26 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-
 // Book struct (Model)
 
 type Book struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Pages  uint `json:"pages"`
-    Author *Author `json:"author"`
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Pages  uint    `json:"pages"`
+	Author *Author `json:"author"`
 }
 
 type Author struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
-	Age  uint `json:"age"`
+	Age  uint   `json:"age"`
 }
 
 var books []Book
 
-/** 
+/**
 Routes Handlers
 **/
-
 // Get all books
 func getBooksController(c *fiber.Ctx) error {
 	return c.JSON(books)
@@ -37,12 +35,13 @@ func getBooksController(c *fiber.Ctx) error {
 // Get single book
 func getBookController(c *fiber.Ctx) error {
 	id := c.Params("id")
-for _, book := range books {
-	if book.ID == id {
-return c.JSON(book)
- }}
+	for _, book := range books {
+		if book.ID == id {
+			return c.JSON(book)
+		}
+	}
 
-return c.Status(404).JSON(fiber.Map{"message": "Book not found"})
+	return c.Status(404).JSON(fiber.Map{"message": "Book not found"})
 }
 
 // Add new book
@@ -56,28 +55,38 @@ func addBookController(c *fiber.Ctx) error {
 	return c.Status(201).JSON(book)
 }
 
+// Delete book
+func deleteBookController(c *fiber.Ctx) error {
+	id := c.Params("id")
+	for index, book := range books {
+		if book.ID == id {
+			books = append(books[:index], books[index+1:]...)
+			return c.Status(200).JSON(fiber.Map{"message": "Book deleted"})
+		}
+	}
+	return c.Status(404).JSON(fiber.Map{"message": "Book not found"})
+}
 
 func main() {
 	// Create new Fiber instance
 	app := fiber.New()
 
-   app.Use(logger.New())
+	app.Use(logger.New())
 
-    books = append(books, Book{ID: "1", Name: "The Alchemist", Pages: 200, Author: 
-	&Author{ID: "1", Name: "Paulo Coelho", Age: 50}})
+	books = append(books, Book{ID: "1", Name: "The Alchemist", Pages: 200,
+		Author: &Author{ID: "1", Name: "Paulo Coelho", Age: 50}})
 
-    books = append(books,Book{ID:"2",Name:"The Monk Who Sold His Ferrari",Pages:300,Author: &Author{ID:"2",Name:"Robin Sharma",Age:40}})
-
+	books = append(books, Book{ID: "2", Name: "The Monk Who Sold His Ferrari", Pages: 300,
+		Author: &Author{ID: "2", Name: "Robin Sharma", Age: 40}})
 
 	// Routes
-   app.Route("/api/v1/books",func(router fiber.Router) {
-	router.Get("/",getBooksController)
-	router.Get("/:id<min(0)>",getBookController)
-	router.Post("/addBook",addBookController)
-	// router.put("/books/:id",updateBookController)
-	// router.delete("/books/:id",deleteBookController)
-
-   })
+	app.Route("/api/v1/books", func(router fiber.Router) {
+		router.Get("/", getBooksController)
+		router.Get("/:id<min(0)>", getBookController)
+		router.Post("/addBook", addBookController)
+		// router.put("/books/:id",updateBookController)
+		router.Delete("/:id", deleteBookController)
+	})
 
 	// app.Get("/books",getBooksController)
 	// app.Get("/books/:id<min(0)>",getBookController)
